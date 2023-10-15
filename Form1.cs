62379e68f97f8b1d01949141ebcb4ab3accd4435
Angellunaa <angelluna996@gmail.com>
2023-10-15 16:48:17 -0600

    Segunda version del proyecto

--- Form1.cs
+++ Form1.cs
@@ -10,14 +10,14 @@ namespace Proyecto_Automatas
         /*  Estado:
             1.- Seleccionar
             2.- Agregar
-            3.-
+            3.- Eliminar
+            4.- Conectar
          */
         private List<Nodo> ListaNodos = new List<Nodo>();
         private List<Arista> ListaAristas = new List<Arista>();
         private bool arrastrando = false;
         private bool Elegido = false;
-        private Nodo nodo = new Nodo(new Point(0, 0));
-        private int cont = 1;
+        private Nodo nodo;
 
         //Metodos
 
@@ -29,6 +29,7 @@ namespace Proyecto_Automatas
         private void Form1_Load(object sender, EventArgs e)
         {
             WindowState = FormWindowState.Maximized;//Abre la pantalla completa al inicializar la aplicacion
+            Editor_Seleccionar.BackColor = Color.SkyBlue;
         }
 
         //----------------------------------------------------------- Botones del editor --------------------------------------------------------------------
@@ -77,19 +78,33 @@ namespace Proyecto_Automatas
                 case 2:
                     //Agrega un Nodo
                     nodo = new Nodo(e.Location);
-                    ListaNodos.Add(nodo);
+                    ListaNodos.Add(new Nodo(e.Location));
                     nodo.Color = Color.Gold;
                     nodo.Dibujar(Pizarra.CreateGraphics());
                     break;
                 case 3:
-                    //Elimina un nodo
-                    foreach (Nodo n in ListaNodos)
+                    //Elimina un nodo y/o arista
+                    foreach (Nodo n in ListaNodos)//Eliminar un nodo al hacer clic
                     {
                         if (n.EstaDentro(e.X, e.Y))
                         {
-                            Rectangle r = new Rectangle(n.Rect.X - 4, n.Rect.Y - 4, 2 * Nodo.radio + 8, 2 * Nodo.radio + 8);
-                            Pizarra.Invalidate(r);
+                            List<Arista> aristasEliminar = ListaAristas.FindAll(a => a.Nodoinicio == n || a.Nodofin == n);//Busca todas las aristas conectadas al nodo
+                            foreach (Arista a in aristasEliminar)//Elimina todas las aristas conectadas al nodo
+                            {
+                                ListaAristas.Remove(a);
+                            }
                             ListaNodos.Remove(n);
+                            Pizarra.Invalidate();
+                            break;
+                        }
+                    }
+
+                    foreach (Arista a in ListaAristas)//Eliminar una arista al hacer clic
+                    {
+                        if (a.EstaDentro(new Point(e.X, e.Y)))
+                        {
+                            ListaAristas.Remove(a);
+                            Pizarra.Invalidate();
                             break;
                         }
                     }
@@ -161,14 +176,13 @@ namespace Proyecto_Automatas
                 // Mover el nodo con el cursor del mouse
                 Rectangle rec = new Rectangle(nodo.Rect.X - Nodo.radio - 50, nodo.Rect.Y - Nodo.radio - 50, Nodo.radio * 5, Nodo.radio * 5);
                 nodo.Mover(e.X, e.Y);
-
                 Pizarra.Invalidate();
             }
         }
 
         private void Pizarra_MouseUp(object sender, MouseEventArgs e)
         {
-            if(estado == 1)
+            if(estado == 1 && arrastrando)
             {
                 nodo.Color = Color.Gold;
                 nodo.Dibujar(Pizarra.CreateGraphics());
--- Nodo.cs
+++ Nodo.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualBasic.Devices;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,7 @@ namespace Proyecto_Automatas
         public const int radio = 40; // Radio del círculo
         private Rectangle rect;
         private Color c;
+        private bool inicial;
 
         //Metodos
 
@@ -23,13 +25,15 @@ namespace Proyecto_Automatas
             this.coordenada = coordenada;
             nombre = "q0";
             rect = new Rectangle(coordenada.X - radio, coordenada.Y - radio, 2 * radio, 2 * radio);
+            inicial = false;
         }
 
-        //Metodos get
+        //Metodos get y set
         public Rectangle Rect{ get {return rect;} }
         public Point Coordenada { get { return coordenada; } }
         public Color Color { set { c = value; } }
 
+        // --------------------------------------------------------- Metodos de la clase nodo -----------------------------------------------------------------
         public void Dibujar(Graphics g)//Dibuja un nodo en la pizarra
         {
             g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
@@ -38,6 +42,9 @@ namespace Proyecto_Automatas
             pen.EndCap = System.Drawing.Drawing2D.LineCap.RoundAnchor;
             g.DrawEllipse(pen, rect);
             g.FillEllipse(new SolidBrush(c), rect);
+            Font font = new Font("Arial", 12, FontStyle.Bold);
+            SizeF textSize = g.MeasureString(nombre, font);
+            g.DrawString(nombre, font, Brushes.Black, coordenada.X - textSize.Width / 2, coordenada.Y - textSize.Width / 2);
         }
 
         public void Mover(int x, int y)//Da la nueva coordenada del nodo que se movio
@@ -53,7 +60,7 @@ namespace Proyecto_Automatas
             return Math.Pow(x - coordenada.X, 2) + Math.Pow(y - coordenada.Y, 2) <= Math.Pow(radio, 2);
         }
 
-        public Arista Conectar(Nodo otroNodo, string valor)
+        public Arista Conectar(Nodo otroNodo, string valor) //Conecta dos nodos y resulta en una arista
         {
             // Crea una arista que conecta este nodo con otro nodo
             return new Arista(this, otroNodo, valor);
