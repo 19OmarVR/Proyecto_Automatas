fde39fb045fa13d97cbf82e9e87813a28e67236a
19OmarVR <omarvrdz64@gmail.com>
2023-10-19 18:43:25 -0600

    implementacion de la tabla Propiedades

--- Diseñador.Designer.cs
+++ Diseñador.Designer.cs
@@ -33,6 +33,14 @@
             toolStripStatusLabel6 = new ToolStripStatusLabel();
             General_Menu_Regresar = new ToolStripStatusLabel();
             Tabla_Propiedades = new TableLayoutPanel();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            TB_Propiedades_Nombre = new TextBox();
+            CB_Propiedades_Color = new ComboBox();
+            TB_Propiedades_Radio = new TextBox();
+            BT_Propiedades_Guardar = new Button();
             Pizzarra = new Panel();
             Menu_Pizzarra = new StatusStrip();
             Pizzarra_Menu_Seleccionar = new ToolStripStatusLabel();
@@ -40,6 +48,7 @@
             Pizzarra_Menu_Conectar = new ToolStripStatusLabel();
             Pizzarra_Menu_Eliminar = new ToolStripStatusLabel();
             statusStrip1.SuspendLayout();
+            Tabla_Propiedades.SuspendLayout();
             Pizzarra.SuspendLayout();
             Menu_Pizzarra.SuspendLayout();
             SuspendLayout();
@@ -50,7 +59,7 @@
             statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel5, toolStripStatusLabel6, General_Menu_Regresar });
             statusStrip1.Location = new Point(0, 0);
             statusStrip1.Name = "statusStrip1";
-            statusStrip1.Size = new Size(800, 22);
+            statusStrip1.Size = new Size(990, 22);
             statusStrip1.TabIndex = 0;
             statusStrip1.Text = "statusStrip1";
             // 
@@ -79,16 +88,119 @@
             // Tabla_Propiedades
             // 
             Tabla_Propiedades.BackColor = Color.Gainsboro;
-            Tabla_Propiedades.ColumnCount = 1;
-            Tabla_Propiedades.ColumnStyles.Add(new ColumnStyle());
+            Tabla_Propiedades.ColumnCount = 2;
+            Tabla_Propiedades.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 126F));
+            Tabla_Propiedades.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 52F));
+            Tabla_Propiedades.Controls.Add(label1, 0, 0);
+            Tabla_Propiedades.Controls.Add(label2, 0, 1);
+            Tabla_Propiedades.Controls.Add(label3, 0, 2);
+            Tabla_Propiedades.Controls.Add(label4, 0, 3);
+            Tabla_Propiedades.Controls.Add(TB_Propiedades_Nombre, 1, 1);
+            Tabla_Propiedades.Controls.Add(CB_Propiedades_Color, 1, 2);
+            Tabla_Propiedades.Controls.Add(TB_Propiedades_Radio, 1, 3);
+            Tabla_Propiedades.Controls.Add(BT_Propiedades_Guardar, 1, 4);
             Tabla_Propiedades.Dock = DockStyle.Right;
-            Tabla_Propiedades.Location = new Point(622, 22);
+            Tabla_Propiedades.Location = new Point(729, 22);
             Tabla_Propiedades.Name = "Tabla_Propiedades";
-            Tabla_Propiedades.RowCount = 1;
-            Tabla_Propiedades.RowStyles.Add(new RowStyle());
+            Tabla_Propiedades.RowCount = 6;
+            Tabla_Propiedades.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            Tabla_Propiedades.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            Tabla_Propiedades.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            Tabla_Propiedades.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            Tabla_Propiedades.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
             Tabla_Propiedades.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-            Tabla_Propiedades.Size = new Size(178, 428);
+            Tabla_Propiedades.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            Tabla_Propiedades.Size = new Size(261, 515);
             Tabla_Propiedades.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Dock = DockStyle.Fill;
+            label1.Font = new Font("Consolas", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(3, 0);
+            label1.Name = "label1";
+            label1.Padding = new Padding(5, 5, 5, 0);
+            label1.Size = new Size(120, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Propiedades";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Dock = DockStyle.Fill;
+            label2.Font = new Font("Consolas", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label2.Location = new Point(3, 30);
+            label2.Name = "label2";
+            label2.Padding = new Padding(5);
+            label2.Size = new Size(120, 30);
+            label2.TabIndex = 1;
+            label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Dock = DockStyle.Fill;
+            label3.Font = new Font("Consolas", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label3.Location = new Point(3, 60);
+            label3.Name = "label3";
+            label3.Padding = new Padding(5);
+            label3.Size = new Size(120, 30);
+            label3.TabIndex = 2;
+            label3.Text = "Color:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Dock = DockStyle.Fill;
+            label4.Font = new Font("Consolas", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label4.Location = new Point(3, 90);
+            label4.Name = "label4";
+            label4.Padding = new Padding(5);
+            label4.Size = new Size(120, 30);
+            label4.TabIndex = 3;
+            label4.Text = "Radio:";
+            // 
+            // TB_Propiedades_Nombre
+            // 
+            TB_Propiedades_Nombre.Dock = DockStyle.Fill;
+            TB_Propiedades_Nombre.Enabled = false;
+            TB_Propiedades_Nombre.Location = new Point(129, 33);
+            TB_Propiedades_Nombre.Name = "TB_Propiedades_Nombre";
+            TB_Propiedades_Nombre.Size = new Size(129, 23);
+            TB_Propiedades_Nombre.TabIndex = 4;
+            // 
+            // CB_Propiedades_Color
+            // 
+            CB_Propiedades_Color.Dock = DockStyle.Fill;
+            CB_Propiedades_Color.Enabled = false;
+            CB_Propiedades_Color.FormattingEnabled = true;
+            CB_Propiedades_Color.Items.AddRange(new object[] { "Gold", "LightGreen", "IndianRed", "LightGray", "LightBlue" });
+            CB_Propiedades_Color.Location = new Point(129, 63);
+            CB_Propiedades_Color.Name = "CB_Propiedades_Color";
+            CB_Propiedades_Color.Size = new Size(129, 23);
+            CB_Propiedades_Color.TabIndex = 5;
+            // 
+            // TB_Propiedades_Radio
+            // 
+            TB_Propiedades_Radio.Dock = DockStyle.Fill;
+            TB_Propiedades_Radio.Enabled = false;
+            TB_Propiedades_Radio.Location = new Point(129, 93);
+            TB_Propiedades_Radio.Name = "TB_Propiedades_Radio";
+            TB_Propiedades_Radio.Size = new Size(129, 23);
+            TB_Propiedades_Radio.TabIndex = 6;
+            // 
+            // BT_Propiedades_Guardar
+            // 
+            BT_Propiedades_Guardar.Dock = DockStyle.Fill;
+            BT_Propiedades_Guardar.Enabled = false;
+            BT_Propiedades_Guardar.Location = new Point(129, 123);
+            BT_Propiedades_Guardar.Name = "BT_Propiedades_Guardar";
+            BT_Propiedades_Guardar.Size = new Size(129, 24);
+            BT_Propiedades_Guardar.TabIndex = 7;
+            BT_Propiedades_Guardar.Text = "Guardar Cambios";
+            BT_Propiedades_Guardar.UseVisualStyleBackColor = true;
+            BT_Propiedades_Guardar.Click += BT_Propiedades_Guardar_Click;
             // 
             // Pizzarra
             // 
@@ -97,7 +209,7 @@
             Pizzarra.Dock = DockStyle.Fill;
             Pizzarra.Location = new Point(0, 22);
             Pizzarra.Name = "Pizzarra";
-            Pizzarra.Size = new Size(622, 428);
+            Pizzarra.Size = new Size(729, 515);
             Pizzarra.TabIndex = 2;
             Pizzarra.MouseClick += Pizzarra_MouseClick;
             // 
@@ -107,7 +219,7 @@
             Menu_Pizzarra.Items.AddRange(new ToolStripItem[] { Pizzarra_Menu_Seleccionar, Pizzarra_Menu_Agregar, Pizzarra_Menu_Conectar, Pizzarra_Menu_Eliminar });
             Menu_Pizzarra.Location = new Point(0, 0);
             Menu_Pizzarra.Name = "Menu_Pizzarra";
-            Menu_Pizzarra.Size = new Size(622, 22);
+            Menu_Pizzarra.Size = new Size(729, 22);
             Menu_Pizzarra.TabIndex = 0;
             Menu_Pizzarra.Text = "statusStrip2";
             // 
@@ -147,7 +259,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(990, 537);
             Controls.Add(Pizzarra);
             Controls.Add(Tabla_Propiedades);
             Controls.Add(statusStrip1);
@@ -155,6 +267,8 @@
             Text = "Diseñador";
             statusStrip1.ResumeLayout(false);
             statusStrip1.PerformLayout();
+            Tabla_Propiedades.ResumeLayout(false);
+            Tabla_Propiedades.PerformLayout();
             Pizzarra.ResumeLayout(false);
             Pizzarra.PerformLayout();
             Menu_Pizzarra.ResumeLayout(false);
@@ -176,5 +290,13 @@
         private ToolStripStatusLabel Pizzarra_Menu_Conectar;
         private ToolStripStatusLabel Pizzarra_Menu_Eliminar;
         private ToolStripStatusLabel General_Menu_Regresar;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox TB_Propiedades_Nombre;
+        private ComboBox CB_Propiedades_Color;
+        private TextBox TB_Propiedades_Radio;
+        private Button BT_Propiedades_Guardar;
     }
 }
--- Diseñador.cs
+++ Diseñador.cs
@@ -17,6 +17,9 @@ namespace Proyecto_Automatas
         private int contadorNodos = 0;
         private bool isDragging = false;
         private Point lastLocation;
+        //variable para almacenar el PictureBox seleccionado
+        private PictureBox selectedPictureBox;
+        public string originalcolor = "";
 
         public Diseñador()
         {
@@ -162,6 +165,9 @@ namespace Proyecto_Automatas
                             isDragging = true;
                             lastLocation = e.Location;
                             PictureBox nodo = sender as PictureBox;
+                            string color = nodo.BackColor.ToString();
+                            originalcolor = color.Replace("Color [", "");
+                            originalcolor = originalcolor.Replace("]", "");
                             nodo.BackColor = Color.SkyBlue;
                             nodo.BringToFront(); // Trae el PictureBox al frente para evitar que quede detrás de otros controles
                         }
@@ -180,7 +186,23 @@ namespace Proyecto_Automatas
             }
             else if (e.Button == MouseButtons.Right)
             {
-                MessageBox.Show("Click Derecho");
+                //Mostrar propiedades
+                switch (estado)
+                {
+                    // Selección
+                    case 1:
+                        isDragging = true;
+                        lastLocation = e.Location;
+                        selectedPictureBox = sender as PictureBox;
+                        selectedPictureBox.BringToFront();
+                        // Actualiza los controles en la tabla de propiedades con las propiedades del PictureBox seleccionado
+                        UpdatePropertyTable(selectedPictureBox);
+                        TB_Propiedades_Nombre.Enabled = true;
+                        CB_Propiedades_Color.Enabled = true;
+                        TB_Propiedades_Radio.Enabled = true;
+                        BT_Propiedades_Guardar.Enabled = true;
+                        break;
+                }
             }
         }
 
@@ -193,7 +215,7 @@ namespace Proyecto_Automatas
                     case 1:
                         isDragging = false;
                         PictureBox nodo = sender as PictureBox;
-                        nodo.BackColor = Color.Gold;
+                        nodo.BackColor = Color.FromName(originalcolor);
                         break;
                 }
             }
@@ -215,6 +237,98 @@ namespace Proyecto_Automatas
 
                 pictureBox.Location = newLocation;
             }
+        }
+
+        // Método para actualizar los controles en la tabla de propiedades
+        private void UpdatePropertyTable(PictureBox pictureBox)
+        {
+            if (pictureBox != null)
+            {
+                // Muestra las propiedades en los controles de la tabla de propiedades
+                TB_Propiedades_Nombre.Text = pictureBox.Name;
+                CB_Propiedades_Color.Text = pictureBox.BackColor.Name;
+                TB_Propiedades_Radio.Text = (pictureBox.Width / 2).ToString();
+
+                // Otros controles y propiedades aquí...
+
+                // Habilita el botón de aplicar cambios
+                BT_Propiedades_Guardar.Enabled = true;
+            }
+        }
+
+        private void BT_Propiedades_Guardar_Click(object sender, EventArgs e)
+        {
+            if (selectedPictureBox != null)
+            {
+                //Aplica los cambios a las propiedades del PictureBox seleccionado
+                try
+                {
+                    if (int.TryParse(TB_Propiedades_Radio.Text, out int radio))
+                    {
+                        int diametro = radio * 2;
+                        selectedPictureBox.Width = diametro;
+                        selectedPictureBox.Height = diametro;
+                    }
+
+                    if (!string.IsNullOrEmpty(CB_Propiedades_Color.Text))
+                    {
+                        selectedPictureBox.BackColor = Color.FromName(CB_Propiedades_Color.Text);
+                    }
+
+                    if (!string.IsNullOrEmpty(TB_Propiedades_Nombre.Text))
+                    {
+                        selectedPictureBox.Name = TB_Propiedades_Nombre.Text;
+                    }
+
+                    // Obtén la ruta de la carpeta raíz del proyecto
+                    string proyectoRaiz = System.IO.Path.GetDirectoryName(System.Windows.Forms.Application.ExecutablePath);
+
+                    // Concatena el nombre de tu imagen a la ruta de la carpeta raíz
+                    string imagePath = System.IO.Path.Combine(proyectoRaiz, "src/Circulo.png");
+
+                    // Cargar la imagen original
+                    Image originalImage = Image.FromFile(imagePath);
+
+                    // Crear una nueva imagen con el texto centrado
+                    Bitmap imageWithText = new Bitmap(originalImage);
+
+                    using (Graphics g = Graphics.FromImage(imageWithText))
+                    {
+                        // Dibujar la imagen original
+                        g.DrawImage(originalImage, new Rectangle(0, 0, imageWithText.Width, imageWithText.Height));
+
+                        // Configurar la fuente y el formato del texto
+                        Font font = new Font("Console", 100, FontStyle.Bold);
+                        string text = TB_Propiedades_Nombre.Text;
+                        SizeF textSize = g.MeasureString(text, font);
+                        Point textLocation = new Point((imageWithText.Width - (int)textSize.Width) / 2, (imageWithText.Height - (int)textSize.Height) / 2);
+
+                        // Dibujar el texto centrado
+                        g.DrawString(text, font, Brushes.Black, textLocation);
+                    }
+
+                    // Mostrar la nueva imagen en el PictureBox
+                    selectedPictureBox.Image = imageWithText;
+                }
+                catch
+                {
+                    MessageBox.Show("Error en los valores de Propiedades");
+                }
+                selectedPictureBox = null;
+                // Limpia los controles de la tabla de propiedades
+                LimpiarPropiedades();
+            }
+        }
+        private void LimpiarPropiedades()
+        {
+            TB_Propiedades_Nombre.Text = string.Empty;
+            CB_Propiedades_Color.Text = string.Empty;
+            TB_Propiedades_Radio.Text = string.Empty;
+            BT_Propiedades_Guardar.Enabled = false;
+
+            TB_Propiedades_Nombre.Enabled = false;
+            CB_Propiedades_Color.Enabled = false;
+            TB_Propiedades_Nombre.Enabled = false;
         }
     }
 }
